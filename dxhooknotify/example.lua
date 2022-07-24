15df2a0b0d92a60e997b4ac1cc083bb4e1fcd757
laagginq <95953670+laagginq@users.noreply.github.com>
2022-07-24 14:30:37 -0700

    Update example.lua

--- dxhooknotify/example.lua
+++ dxhooknotify/example.lua
@@ -1,2 +1,2 @@
-local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/alingiaer/DxHookNotify/8a22b99baaca63f09e106bd55208756267b1cd59/sad", true))()
+local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/dxhooknotify/src.lua", true))()
 l:Notify("Title","Descript",10)
