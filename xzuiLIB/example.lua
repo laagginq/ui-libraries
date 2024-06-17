275817f9e04cedbf43407abffdf3ad45c0986ed5
xz <95953670+laagginq@users.noreply.github.com>
2024-06-17 12:28:19 -0700

    Update example.lua

--- xzuiLIB/example.lua
+++ xzuiLIB/example.lua
@@ -13,18 +13,26 @@ Tab1:Button({Text = "Button", Callback = function()
 	print("Button clicked")
 end})
 
-Tab1:Dropdown({Text = "Dropdown", Value = 1, Values = {"1","2","3"}, Callback = function(v)
+Tab1:Dropdown({Flag = "Dropdown", Text = "Dropdown", Value = 1, Values = {"1","2","3"}, Callback = function(v)
 	print(v.." Selected")
 end})
 
-Tab1:Slider({Text = "Slider", Min = 25, Def = 50, Max = 100, Suffix = "%", Callback = function(v)
+Tab1:Slider({Flag = "Slider", Text = "Slider", Min = 25, Def = 50, Max = 100, Suffix = "%", Callback = function(v)
 	print(v)
 end})
 
-Tab1:Toggle({Text = "Toggle", Default = true, Callback = function(v)
+Tab1:Toggle({Flag = "Toggle", Text = "Toggle", Default = true, Callback = function(v)
 	print(v)
 end})
 
-Tab1:Input({Text = "Input", Default = "Yea nigga", PlaceHolder = "Input text here", Callback = function(v)
+Tab1:Input({Flag = "Input", Text = "Input", Default = "Yea nigga", PlaceHolder = "Input text here", Callback = function(v)
 	print(v)
 end})
+
+-- // Print all the flags 
+
+task.wait(0.1)
+
+for i,v in pairs(Flags) do 
+    print(i,v)
+end
